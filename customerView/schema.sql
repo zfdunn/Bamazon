1191a7f162a2528a9bf97af507eb1710a710a19f
Zach Dunn <zfdunn@comcast.net>
2019-09-25 19:54:40 -0400

    created customerView directory

--- customerView/schema.sql
+++ customerView/schema.sql
@@ -7,9 +7,9 @@ CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(250) NULL,
     department_name VARCHAR(250) NULL,
-    price DECIMAL NULL,
+    price DECIMAL (10, 2),
     stock_quantity INT NULL,
-    PRIMARY KEY (item_id)
+    -- PRIMARY KEY (item_id)
 );
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
